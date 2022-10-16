5b09895e3283db6b15e903f6b67d362c6218fe54
Vinicius Russo <56481183+viniciussrusso@users.noreply.github.com>
2022-10-16 09:46:53 +0100

    Portfolio added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { NavBar } from "./components/NavBar";
 import { Home } from "./components/Home";
 import { SocialLinks } from "./components/SocialLinks";
 import { Above } from "./components/Above";
+import { Portfolio } from "./components/Portfolio";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
-      <SocialLinks />
       <Above />
+      <Portfolio />
+      <SocialLinks />
     </div>
   );
 }
