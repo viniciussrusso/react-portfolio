1472147d4bb3be9559284c4f57514dcc20e7b8e4
Vinicius Russo <56481183+viniciussrusso@users.noreply.github.com>
2022-11-13 12:56:16 +0000

    changed buttons

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { IoIosGlobe } from "react-icons/io";
+import { AiOutlineGithub } from "react-icons/ai";
 import { portfolios } from "../utils";
 
 export const Portfolio = () => {
@@ -19,20 +21,32 @@ export const Portfolio = () => {
           key=""
           className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0"
         >
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, demo, code }) => (
             <div key={id} className="shadow-md shadow-cyan-500 rounded-lg">
               <img
                 src={src}
                 alt=""
                 className="rounded-md duration-200 hover:scale-105"
               />
-              <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105  hover:text-cyan-500">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105  hover:text-cyan-500">
-                  Code
-                </button>
+              <div className="py-2 flex justify-around">
+                <a
+                  href={demo}
+                  className="text-white w-[48%] text-sm inline-flex items-center text-center bg-slate-700 rounded-lg p-2 hover:scale-105 duration-500 hover:bg-sky-700"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <IoIosGlobe className="pr-1" size="20px" />
+                  Project Online
+                </a>
+                <a
+                  href={code}
+                  className="text-white w-[48%] text-sm inline-flex items-center text-center bg-slate-700 rounded-lg p-2 hover:scale-105 duration-500 hover:bg-sky-700"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <AiOutlineGithub className="pr-1" size="20px" />
+                  Code Link
+                </a>
               </div>
             </div>
           ))}
